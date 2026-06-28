09e3b3d4532b287bd16d5f13328b5ffc3a98d40a
Ryan Rackemann <ryanrackemann@gmail.com>
2026-06-28 01:11:31 -0700

    Auto-generated commit

--- src/app/pages/registry/registry.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-registry',
-  templateUrl: './registry.component.html',
-  styleUrl: './registry.component.scss'
-})
-export class RegistryComponent {}
